216a984f003ba92a85226b9f7c6c74de472a807f
Plopmenz <plopmenz@gmail.com>
2024-01-29 12:01:49 +0100

    test improvement

--- test/ERC721CountTrustlessManagement.t.sol
+++ test/ERC721CountTrustlessManagement.t.sol
@@ -15,10 +15,10 @@ contract ERC721CountTrustlessManagementTest is Test {
         trustlessManagement = new ERC721CountTrustlessManagement(collection);
     }
 
-    function test_hasRole(address _account, uint256 _minTokens, uint256 _balance) external {
+    function test_hasRole(address _account, uint256 _minTokens, uint8 _balance) external {
+        // Balance as uint8, as otherwise the test will take forever (and be killed after a while)
         vm.assume(_account.code.length == 0); // Smart contracts needs to support IERC721Receiver
         vm.assume(_account != address(0)); // Not allowed to send tokens to zero address
-        vm.assume(_balance < type(uint8).max); // Otherwise the test will take forever (and be killed after a while)
         for (uint256 i; i < _balance; i++) {
             collection.setOwner(_account, i);
         }
